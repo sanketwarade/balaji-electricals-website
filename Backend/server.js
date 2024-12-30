4a469790580b87eed5a7e53ad22cfd651d281f44
sanket <sanketwarade456@gmail.com>
2024-12-31 00:15:29 +0530

    Updated Backend/server.js

--- Backend/server.js
+++ Backend/server.js
@@ -201,7 +201,7 @@ app.post('/submit-quoteForm', [
   body('email').isEmail().normalizeEmail(),
   body('contact').isLength({min:10, max:100}).trim(),
   body('message').trim().escape().isLength({ min: 10, max: 100 }),
-  body('company').trim.escape().isLength({min:3, max:50}),
+  body('company').trim().escape().isLength({min: 3, max: 50}),
   body('machines').trim.escape().isIn(['Mig Welding Machine', 'Tig Welding Machine', 'SPM Welding Machine', 'Rotary Positioner','X-Y Linear Slides','Spare Parts','Control Panels']) .withMessage('Invalid machine selection.')
 ], 
   (req, res) => {
