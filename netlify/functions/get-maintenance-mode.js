09118cedf61c1d2380ad1b511cccfec8051b8a74
sanket <sanketwarade456@gmail.com>
2025-01-08 12:12:20 +0530

    Update netlify/functions/get-maintenance-mode.js

--- netlify/functions/get-maintenance-mode.js
+++ netlify/functions/get-maintenance-mode.js
@@ -6,4 +6,16 @@ exports.handler = async () => {
       body: JSON.stringify({ maintenanceMode })
     };
   };
+
+  //add below code to index html in head section below title by removing comment if you want to use the maintenance mode in your app
+  //<script>
+        //fetch('/.netlify/functions/get-maintenance-mode')
+        //  .then(response => response.json())
+          //.then(data => {
+          //  if (data.maintenanceMode === 'FALSE') {
+        //      window.location.href = "/maintenance.html";
+          //  }
+        //  })
+        //  .catch(error => console.error('Error fetching maintenance mode:', error));
+     // </script>
   
